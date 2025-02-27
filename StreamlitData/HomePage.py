53cad10c68c76c4126c7c7083e5de1f470ef7b24
shokiche92 <39572009+shokiche92@users.noreply.github.com>
2025-02-27 18:55:31 -0300

    Update HomePage.py

--- StreamlitData/HomePage.py
+++ StreamlitData/HomePage.py
@@ -7,3 +7,18 @@ st.title('Oficina Chile & Perú')
 
 st.subheader("Procesamiento de datos - Cierre Financiero 2025")
 options=st.sidebar.radio('Select:',['HomePage','Capturing_Oneliner','Report_DRO'])
+def HomePage():
+    st.header('Welcome')
+def Capturing_Oneliner():
+    st.header('Oneliner')
+def Report_DRO():
+    st.header('DRO')
+
+#Navigation Option
+if options== 'Home':
+    HomePage()
+elif options=='Chile':
+    Capturing_Oneliner()
+elif options=='Peru' :
+    Report_DRO()
+    
