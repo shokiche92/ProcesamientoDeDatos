50c2fd29c56e3fc0c4deb58399e743cd6e162542
shokiche92 <39572009+shokiche92@users.noreply.github.com>
2025-02-27 17:58:49 -0300

    Update HomePage.py

--- StreamlitData/HomePage.py
+++ StreamlitData/HomePage.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-#Python -m streamlit run StreamlitData/HomePage.py  
+#Python -m streamlit run StreamlitData/HomePage.py  https://github.com/shokiche92/ProcesamientoDeDatos/blob/main/StreamlitData/HomePage.py
 
 st.set_page_config(page_title='Multipage App')
 st.title('Oficina Chile & Perú')
-#st.sidebar.success('Select a page above.')
+st.sidebar.success('Select a page above.')
 st.subheader("Procesamiento de datos - Cierre Financiero 2025")
