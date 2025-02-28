79cbe4cdcdc57df4f3ec20e2be2734cd92525b90
shokiche92 <39572009+shokiche92@users.noreply.github.com>
2025-02-28 10:20:34 -0300

    Delete StreamlitData directory

--- StreamlitData/Pages/AboutProject.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-#Python -m streamlit run StreamlitData/HomePage.py  
-
-st.set_page_config(page_title='Capturando Datos')
-st.title('Oficina Chile & Perú')
-#st.sidebar.success('Select a page above.')
-st.subheader("Procesamiento de datos - Cierre Financiero 2025")
